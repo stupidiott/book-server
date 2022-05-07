3acfbc666e8d1af04272fbfb7cd13190aaf22d80
Dong He <hedong2001@outlook.com>
2022-05-07 22:48:45 +0800

    将copy的信息加入到数据库

--- edu_book.sql
+++ edu_book.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80025
  File Encoding         : 65001
 
- Date: 07/05/2022 01:15:01
+ Date: 07/05/2022 22:48:23
 */
 
 SET NAMES utf8mb4;
@@ -37,8 +37,8 @@ CREATE TABLE `edu_account`  (
 -- ----------------------------
 -- Records of edu_account
 -- ----------------------------
-INSERT INTO `edu_account` VALUES (1, 'Librarian', '21232f297a57a5a743894a0e4a801fc3', 1, 1, '2021-07-07 20:46:08', '2022-05-07 00:35:31', 0);
-INSERT INTO `edu_account` VALUES (2, 'User', '6512bd43d9caa6e02c990b0a82652dca', 1, 4, '2022-04-12 23:59:00', '2022-04-30 19:58:52', 0);
+INSERT INTO `edu_account` VALUES (1, 'Librarian', '21232f297a57a5a743894a0e4a801fc3', 1, 1, '2021-07-07 20:46:08', '2022-05-07 22:44:42', 0);
+INSERT INTO `edu_account` VALUES (2, 'User', '6512bd43d9caa6e02c990b0a82652dca', 1, 4, '2022-04-12 23:59:00', '2022-05-07 22:44:16', 0);
 INSERT INTO `edu_account` VALUES (8, 'User2', 'b6d767d2f8ed5d21a44b0e5886680cb9', 0, 4, '2022-04-13 19:22:18', '2022-04-13 23:57:56', 0);
 INSERT INTO `edu_account` VALUES (9, 'test', '202cb962ac59075b964b07152d234b70', 1, 4, '2022-04-14 09:20:14', '2022-04-14 09:20:37', 0);
 INSERT INTO `edu_account` VALUES (10, '123', 'e10adc3949ba59abbe56e057f20f883e', 1, 1, '2022-04-19 01:10:53', '2022-04-26 23:59:20', 0);
@@ -81,6 +81,21 @@ INSERT INTO `edu_account_login` VALUES (385, '1651843405826', 'Librarian', '{\"a
 INSERT INTO `edu_account_login` VALUES (386, '1651849488582', 'Librarian', '{\"accountType\":1,\"createTime\":1625661968000,\"debt\":0,\"id\":1,\"lastLoginTime\":1651843406000,\"password\":\"21232f297a57a5a743894a0e4a801fc3\",\"status\":1,\"username\":\"Librarian\"}', '2022-05-06 23:04:49');
 INSERT INTO `edu_account_login` VALUES (387, '1651849819717', 'Librarian', '{\"accountType\":1,\"createTime\":1625661968000,\"debt\":0,\"id\":1,\"lastLoginTime\":1651849489000,\"password\":\"21232f297a57a5a743894a0e4a801fc3\",\"status\":1,\"username\":\"Librarian\"}', '2022-05-06 23:10:20');
 INSERT INTO `edu_account_login` VALUES (388, '1651854930666', 'Librarian', '{\"accountType\":1,\"createTime\":1625661968000,\"debt\":0,\"id\":1,\"lastLoginTime\":1651849820000,\"password\":\"21232f297a57a5a743894a0e4a801fc3\",\"status\":1,\"username\":\"Librarian\"}', '2022-05-07 00:35:31');
+INSERT INTO `edu_account_login` VALUES (389, '1651923958960', 'User', '{\"accountType\":4,\"createTime\":1649779140000,\"debt\":0,\"id\":2,\"lastLoginTime\":1651319932000,\"password\":\"6512bd43d9caa6e02c990b0a82652dca\",\"status\":1,\"username\":\"User\"}', '2022-05-07 19:45:59');
+INSERT INTO `edu_account_login` VALUES (390, '1651929059808', 'Librarian', '{\"accountType\":1,\"createTime\":1625661968000,\"debt\":0,\"id\":1,\"lastLoginTime\":1651854931000,\"password\":\"21232f297a57a5a743894a0e4a801fc3\",\"status\":1,\"username\":\"Librarian\"}', '2022-05-07 21:11:00');
+INSERT INTO `edu_account_login` VALUES (391, '1651929626282', 'User', '{\"accountType\":4,\"createTime\":1649779140000,\"debt\":0,\"id\":2,\"lastLoginTime\":1651923959000,\"password\":\"6512bd43d9caa6e02c990b0a82652dca\",\"status\":1,\"username\":\"User\"}', '2022-05-07 21:20:26');
+INSERT INTO `edu_account_login` VALUES (392, '1651931787527', 'Librarian', '{\"accountType\":1,\"createTime\":1625661968000,\"debt\":0,\"id\":1,\"lastLoginTime\":1651929060000,\"password\":\"21232f297a57a5a743894a0e4a801fc3\",\"status\":1,\"username\":\"Librarian\"}', '2022-05-07 21:56:28');
+INSERT INTO `edu_account_login` VALUES (393, '1651932967094', 'User', '{\"accountType\":4,\"createTime\":1649779140000,\"debt\":0,\"id\":2,\"lastLoginTime\":1651929626000,\"password\":\"6512bd43d9caa6e02c990b0a82652dca\",\"status\":1,\"username\":\"User\"}', '2022-05-07 22:16:07');
+INSERT INTO `edu_account_login` VALUES (394, '1651932979927', 'Librarian', '{\"accountType\":1,\"createTime\":1625661968000,\"debt\":0,\"id\":1,\"lastLoginTime\":1651931788000,\"password\":\"21232f297a57a5a743894a0e4a801fc3\",\"status\":1,\"username\":\"Librarian\"}', '2022-05-07 22:16:20');
+INSERT INTO `edu_account_login` VALUES (395, '1651933038269', 'User', '{\"accountType\":4,\"createTime\":1649779140000,\"debt\":0,\"id\":2,\"lastLoginTime\":1651932967000,\"password\":\"6512bd43d9caa6e02c990b0a82652dca\",\"status\":1,\"username\":\"User\"}', '2022-05-07 22:17:18');
+INSERT INTO `edu_account_login` VALUES (396, '1651933961569', 'Librarian', '{\"accountType\":1,\"createTime\":1625661968000,\"debt\":0,\"id\":1,\"lastLoginTime\":1651932980000,\"password\":\"21232f297a57a5a743894a0e4a801fc3\",\"status\":1,\"username\":\"Librarian\"}', '2022-05-07 22:32:42');
+INSERT INTO `edu_account_login` VALUES (397, '1651934299432', 'User', '{\"accountType\":4,\"createTime\":1649779140000,\"debt\":0,\"id\":2,\"lastLoginTime\":1651933038000,\"password\":\"6512bd43d9caa6e02c990b0a82652dca\",\"status\":1,\"username\":\"User\"}', '2022-05-07 22:38:19');
+INSERT INTO `edu_account_login` VALUES (398, '1651934332332', 'Librarian', '{\"accountType\":1,\"createTime\":1625661968000,\"debt\":0,\"id\":1,\"lastLoginTime\":1651933962000,\"password\":\"21232f297a57a5a743894a0e4a801fc3\",\"status\":1,\"username\":\"Librarian\"}', '2022-05-07 22:38:52');
+INSERT INTO `edu_account_login` VALUES (399, '1651934461701', 'User', '{\"accountType\":4,\"createTime\":1649779140000,\"debt\":0,\"id\":2,\"lastLoginTime\":1651934299000,\"password\":\"6512bd43d9caa6e02c990b0a82652dca\",\"status\":1,\"username\":\"User\"}', '2022-05-07 22:41:02');
+INSERT INTO `edu_account_login` VALUES (400, '1651934485392', 'Librarian', '{\"accountType\":1,\"createTime\":1625661968000,\"debt\":0,\"id\":1,\"lastLoginTime\":1651934332000,\"password\":\"21232f297a57a5a743894a0e4a801fc3\",\"status\":1,\"username\":\"Librarian\"}', '2022-05-07 22:41:25');
+INSERT INTO `edu_account_login` VALUES (401, '1651934621844', 'User', '{\"accountType\":4,\"createTime\":1649779140000,\"debt\":0,\"id\":2,\"lastLoginTime\":1651934462000,\"password\":\"6512bd43d9caa6e02c990b0a82652dca\",\"status\":1,\"username\":\"User\"}', '2022-05-07 22:43:42');
+INSERT INTO `edu_account_login` VALUES (402, '1651934655629', 'User', '{\"accountType\":4,\"createTime\":1649779140000,\"debt\":0,\"id\":2,\"lastLoginTime\":1651934622000,\"password\":\"6512bd43d9caa6e02c990b0a82652dca\",\"status\":1,\"username\":\"User\"}', '2022-05-07 22:44:16');
+INSERT INTO `edu_account_login` VALUES (403, '1651934681891', 'Librarian', '{\"accountType\":1,\"createTime\":1625661968000,\"debt\":0,\"id\":1,\"lastLoginTime\":1651934485000,\"password\":\"21232f297a57a5a743894a0e4a801fc3\",\"status\":1,\"username\":\"Librarian\"}', '2022-05-07 22:44:42');
 
 -- ----------------------------
 -- Table structure for edu_account_menu
@@ -122,8 +137,8 @@ CREATE TABLE `edu_book`  (
 -- ----------------------------
 -- Records of edu_book
 -- ----------------------------
-INSERT INTO `edu_book` VALUES (24, '6', 'Real-time rendering', 'Tomas Möller', 'C区', '3F', '古典', 'A K Peters, Ltc.', 66, NULL, 1, '2022-05-07 00:43:45', 6);
-INSERT INTO `edu_book` VALUES (27, '65', 'Against nature =', 'Joris-Karl Huysmans', 'B区', '2F', '古典', 'Oxford University Press', 66, NULL, 0, '2022-05-07 00:47:59', 6);
+INSERT INTO `edu_book` VALUES (34, '9780451159274', 'IT', 'Stephen King', 'B区', '2F', '名著', 'New American Library', 5, 'Derry: A small city in Maine, place as hauntingly familiar as your own hometown, only in Derry the haunting is real...\r\n\r\nThey were seven teenagers when they first stumbled upon the horror. Now they are grown-up men and women who have gone out into the big world to gain success and happiness. But none of them can withstand the force that has drawn them back to Derry to face the nightmare without an end, and the evil without a name...\r\n--back cover', 0, '2022-05-07 21:14:25', 5);
+INSERT INTO `edu_book` VALUES (35, '9780450032202', 'The Shining', 'Stephen King', 'A区', '1F', '古典', 'New English Library', 10, NULL, 0, '2022-05-07 21:16:16', 11);
 
 -- ----------------------------
 -- Table structure for edu_borrow_book
@@ -145,6 +160,39 @@ CREATE TABLE `edu_borrow_book`  (
 -- ----------------------------
 -- Records of edu_borrow_book
 -- ----------------------------
+INSERT INTO `edu_borrow_book` VALUES (100, '9780450032202', 'User', '2022-05-07 22:43:50', '2022-05-17 22:43:50', '2022-05-07 22:43:51', 1, '2022-05-07 22:45:05', 0);
+INSERT INTO `edu_borrow_book` VALUES (101, '9780451159274', 'User', '2022-05-07 22:43:55', '2022-05-17 22:43:55', '2022-05-07 22:43:56', 1, '2022-05-07 22:44:51', 0);
+
+-- ----------------------------
+-- Table structure for edu_copy
+-- ----------------------------
+DROP TABLE IF EXISTS `edu_copy`;
+CREATE TABLE `edu_copy`  (
+  `book_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `book_no` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `flag` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`book_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of edu_copy
+-- ----------------------------
+INSERT INTO `edu_copy` VALUES ('97804500322021', '9780450032202', '0', '');
+INSERT INTO `edu_copy` VALUES ('978045003220210', '9780450032202', '0', '');
+INSERT INTO `edu_copy` VALUES ('97804500322022', '9780450032202', '0', '');
+INSERT INTO `edu_copy` VALUES ('97804500322023', '9780450032202', '0', '');
+INSERT INTO `edu_copy` VALUES ('97804500322024', '9780450032202', '0', '');
+INSERT INTO `edu_copy` VALUES ('97804500322025', '9780450032202', '0', '');
+INSERT INTO `edu_copy` VALUES ('97804500322026', '9780450032202', '0', '');
+INSERT INTO `edu_copy` VALUES ('97804500322027', '9780450032202', '0', '');
+INSERT INTO `edu_copy` VALUES ('97804500322028', '9780450032202', '0', '');
+INSERT INTO `edu_copy` VALUES ('97804500322029', '9780450032202', '0', '');
+INSERT INTO `edu_copy` VALUES ('97804511592741', '9780451159274', '0', '');
+INSERT INTO `edu_copy` VALUES ('97804511592742', '9780451159274', '0', '');
+INSERT INTO `edu_copy` VALUES ('97804511592743', '9780451159274', '0', '');
+INSERT INTO `edu_copy` VALUES ('97804511592744', '9780451159274', '0', '');
+INSERT INTO `edu_copy` VALUES ('97804511592745', '9780451159274', '0', '');
 
 -- ----------------------------
 -- Table structure for edu_parameter
